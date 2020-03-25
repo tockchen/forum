d5641d3a3596649e2870ca45e6748bfe68e5afcd
tockchen <1162797060@qq.com>
2020-03-25 14:30:02 +0800

    修改若干BUG

--- src/main/java/work/ccpw/community/controller/AuthorizeController.java
+++ src/main/java/work/ccpw/community/controller/AuthorizeController.java
@@ -54,7 +54,6 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.getName());
         if (githubUser != null && githubUser.getId() != null) {
             User user = new User();
             String token = UUID.randomUUID().toString();
